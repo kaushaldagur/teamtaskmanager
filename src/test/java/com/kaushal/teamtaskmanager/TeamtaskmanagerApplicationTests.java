5caf3c3e6e4227813001e94e0225b815faad78ec
kaushaldagur <kaushaldagur360@gmail.com>
2026-05-19 00:37:25 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/kaushal/teamtaskmanager/TeamtaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaushal.teamtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
